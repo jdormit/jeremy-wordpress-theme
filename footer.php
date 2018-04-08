c4e8f23667fbd9fbd866767ead54a1e5ee2b0398
Jeremy Dormitzer <jdormitzer@hubspot.com>
2018-04-08 08:55:05 -0400

    Remove footer attributions

--- footer.php
+++ footer.php
@@ -1,13 +1,7 @@
         </div><!-- .site-content -->
 
         <footer id="colophon" class="site-footer" role="contentinfo">
-            <div class="site-info container">
-            <?php /* translators: %s: WordPress */ ?>
-                <a href="<?php echo esc_url( esc_attr__( 'https://wordpress.org/', 'simplent' ) ) ?>"><?php printf( esc_attr__( 'Proudly powered by %s', 'simplent' ), 'WordPress' ); ?></a>
-
-                <?php /* translators: %s: theme author */ ?>
-                <a class="theme-credit" href="<?php echo esc_url( esc_attr__( 'https://abdulrafay.me/', 'simplent' ) ) ?>" target="_blank"><?php printf( esc_attr__( 'Based on %1$s by %2$s', 'simplent'), 'Simplent', 'Rafay' ); ?></a>
-            </div>
+            <div class="site-info container"></div>
         </footer>
 
     </div><!-- site-inner -->
