2bade2d51ea07166d7d76e8843968f4bd00ef34b
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-07-27 15:56:38 -0400

    Basic hamburger menu on mobile

--- header.php
+++ header.php
@@ -41,7 +41,11 @@
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'jeremy-wordpress-theme' ); ?></button>
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false">
+                <img class="icon"
+                     src="<?php echo get_template_directory_uri() . '/icons/menu.svg'?>"
+                     alt="menu">
+            </button>
 			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
