7d6ed016db0d8e52076152c647001b78a46d5bad
Jeremy Dormitzer <jeremy.dormitzer@gmail.com>
2018-07-27 10:34:24 -0400

    Add copyright to footer

--- footer.php
+++ footer.php
@@ -15,18 +15,8 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'jeremy-wordpress-theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'jeremy-wordpress-theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'jeremy-wordpress-theme' ), 'jeremy-wordpress-theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+            <span class="site-copyright">&#169; Jeremy Dormitzer <?php echo date('Y');?></span>
+        </div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
